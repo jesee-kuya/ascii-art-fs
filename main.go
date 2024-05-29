98e46b6e69009083568323d6b123060bd18be32f
jkuya <kuyajesee@proton.me>
2024-05-29 18:44:00 +0300

    Added conditional check for output names

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
 	"ascii/ascii"
 )
+
 // main function is used to read the banner file and print the ascii art
 // based on the arguments passed
 func main() {
-	for _ , v := range os.Args {
+	for _, v := range os.Args {
 		if v == "--color" || v == "-color" {
 			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
-				return
+			return
 		}
 	}
 	var filename string
@@ -20,10 +22,33 @@ func main() {
 	var lettersTocolor string
 	var colorCode string
 
-	flag.StringVar(&filename, "filename", "standard", "name for the files")
 	flag.StringVar(&colorflag, "color", "reset", "color for color input")
 	flag.Parse()
-	 argsPassed:= flag.Args()
+	argsPassed := flag.Args()
+
+	if len(argsPassed) == 3 {
+		if !ascii.IsFlagPassed("color") {
+			fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
+			return
+		}
+		filename = argsPassed[2]
+		argsPassed = argsPassed[:2]
+	} else if len(argsPassed) == 2 {
+		if ascii.IsFlagPassed("color") {
+			_, err := ascii.GetFileName(argsPassed[1])
+			if err != nil {
+				filename = "standard"
+			} else {
+				filename = argsPassed[1]
+				argsPassed = argsPassed[:1]
+			}
+		} else {
+			filename = argsPassed[1]
+			argsPassed = argsPassed[:1]
+		}
+	} else if len(argsPassed) == 1 {
+		filename = "standard"
+	}
 
 	bannerContent, err := ascii.GetFileName(filename)
 	if err != nil {
